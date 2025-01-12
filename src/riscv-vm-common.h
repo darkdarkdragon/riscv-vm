f56b6fe635d4f505fe34b0b41bc2a283b2899cd2
Ivan Tivonenko <darkdarkdragon@gmail.com>
2025-01-12 09:16:37 -0500

    refactor

--- src/riscv-vm-common.h
+++ src/riscv-vm-common.h
@@ -1,12 +1,30 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdlib.h>
 
 #define SYS_write 64
 #define INT_MIN_HEX 0x80000000
 
 // 32 registers, 32 bits each
 static const int REG_MEM_SIZE = 32 * 4;
+
+// io devices
+static const uint32_t UART_OUT_REGISTER = 0x10000000;
+
+static const size_t alignment = 64;
+
+static const int ERR_OUT_OF_MEM = 124;
+static const int ERR_UNIMPLEMENTED_OPCODE = 123;
+static const int ERR_INVALID_MEMORY_ACCESS = 122;
+static const int ERR_MISALIGNED_MEMORY_ACCESS = 121;
+static const int ERR_UNIMPLEMENTED_MAGIC_SYSCALL = 120;
+static const int ERR_EBREAK = 100;
+static const int ERR_WFI = 99;
+
+// magic memory addres to communicate with the host
+static const uint32_t tohost = 0x1000;
+static const uint32_t fromhost = 0x1040;
 
 
 static const char *reg_func_name[] __attribute__((unused)) = {
--- src/riscv-vm-optimized-2.c
+++ src/riscv-vm-optimized-2.c
@@ -17,23 +17,7 @@
 // static const int VM_MEMORY = 1048576;
 static const int VM_MEMORY = 1048576 * 16;
 
-// io devices
-static const uint32_t UART_OUT_REGISTER = 0x10000000;
-
-static const size_t alignment = 64;
 static const size_t work_mem_size = VM_MEMORY * 1; // size of memory to allocate
-
-static const int ERR_OUT_OF_MEM = 124;
-static const int ERR_UNIMPLEMENTED_OPCODE = 123;
-static const int ERR_INVALID_MEMORY_ACCESS = 122;
-static const int ERR_MISALIGNED_MEMORY_ACCESS = 121;
-static const int ERR_UNIMPLEMENTED_MAGIC_SYSCALL = 120;
-static const int ERR_EBREAK = 100;
-static const int ERR_WFI = 99;
-
-// magic memory addres to communicate with the host
-static const uint32_t tohost = 0x1000;
-static const uint32_t fromhost = 0x1040;
 
 #if LOG_TRACE
 static void dbg_dump_registers_short(uint32_t *reg);
